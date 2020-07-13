b71a497273dd32511d4f94fe5ed37e449b924619
utshav <utshavtimsina123@gmail.com>
2020-07-13 22:46:59 +0545

    Working Jwt with local username password, call database in Customuserdetails class

---
+++ src/main/java/com/famas/demo/services/CustomUserByUsername.java
@@ -0,0 +1,32 @@
+package com.famas.demo.services;
+
+ 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.famas.demo.model.Role;
+import com.famas.demo.model.User;
+import com.famas.demo.util.CustomUserDetails;
+
+@Service
+public class CustomUserByUsername {
+	
+	CustomUserDetails userDetails = new CustomUserDetails();
+	private Role role;
+	private User user;
+	
+	public CustomUserByUsername() {
+		
+	}
+	public CustomUserDetails getUser(String username) {
+		//call database if the user exists or not if exists return user else return Usernotfoundexception 
+		 role = new Role(1,"ROLE_ADMIN");
+		 user = new User(1,"utshav","pwd",role);
+		 
+		userDetails.setUser(user);
+		return  userDetails;
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/famas/demo/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.famas.demo.services;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+	@Autowired
+	CustomUserByUsername user;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		Logger log = LoggerFactory.getLogger(this.getClass());
+		UserDetails userExists = user.getUser(username);
+		if( userExists!= null) {
+			return userExists ;
+		}
+		throw new UsernameNotFoundException("Unautorized");
+	}
+
+}
